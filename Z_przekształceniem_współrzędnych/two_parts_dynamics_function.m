18d25c3f8fc4a2ecc023b1f72a838f75f6deef09
Marta Krauze <tygrysmws@gmail.com>
2023-01-11 10:54:10 +0100

    Trochę sprzątania

--- Z_przekształceniem_współrzędnych/two_parts_dynamics_function.m
+++ Z_przekształceniem_współrzędnych/two_parts_dynamics_function.m
@@ -8,6 +8,8 @@ function r=two_parts_dynamics_function(x,u)
     global S1
     global S2
 
+    u=[0;0;0];
+
    g=[0;grav*m1;u(1)-u(2);0;grav*m2;u(2)];
  
    thetadot=[x(3);x(4)];
@@ -61,64 +63,6 @@ function r=d_dot(part, joint, S1_g, S2_g, fidot)
    r=r+S2_g(:,part)*fidot(part);
 endfunction
 
-function r=f(x,u)
-    global I
-    global Q 
-    global M
-    global s_A1
-    global s_B1
-    global s_B2
-    global grav
-    global m1
-    global m2
-
-    #g=[0;grav*m1;0;0;grav*m2;0];
-
-    g=[0;grav*m1;u(1)-u(2);0;grav*m2;u(2)];
-
-    #[0;grav*m1;mom1-mom2;0;grav*m2;mom2]
-
-
-    fi1=x(1);
-    fi2=x(2)+x(1);
-    fidot1=x(3);
-    fidot2=x(4)+x(3);  
-    thetadot=[x(3);x(4)];
-
-    s_A1_g=R(fi1)*s_A1;
-    s_B1_g=R(fi1)*s_B1;
-    s_B2_g=R(fi2)*s_B2;
-
-    D=[I, Q * s_A1_g, zeros(2,3);
-       I, Q * s_B1_g, -I, -Q * s_B2_g];
-    
-
-    d_11=-Q * s_A1_g;
-    d_21=Q*(-s_A1_g+s_B1_g-s_B2_g);
-    d_22=-Q*s_B2_g;
-
-    B=[d_11, zeros(2,1);
-          1, 0;
-       d_21, d_22;
-          1, 1];
-    
-    #D*B   
-
-    d_11_dot=s_A1_g*fidot1;
-    d_21_dot=-(-s_A1_g*fidot1+s_B1_g*fidot1-s_B2_g*fidot2);
-    d_22_dot=s_B2_g*fidot2;
-
-    Bdot=[d_11_dot, zeros(2,1);
-          0, 0;
-       d_21_dot, d_22_dot;
-          0, 0];
-
-    M_new=B' * M * B;
-    g_new=B' * (g - M * Bdot * thetadot);
-
-    r1=x(3);
-    r2=x(4);
-    r3=M_new\g_new;
-    r=[r1;r2;r3];
-
+function rot=R(fi)
+    rot=[cos(fi), -sin(fi); sin(fi), cos(fi)];
 endfunction
